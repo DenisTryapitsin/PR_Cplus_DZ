280403567f72cccaa4f79c716721142e61d2cadd
Denis <oumniy@gmail.com>
2023-04-27 21:56:35 +0300

    написал код

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число: ");
+int a;
+a = int.Parse(Console.ReadLine());
+if (a%2==0) {
+  System.Console.WriteLine("число четное");
+  }
+else {
+  System.Console.WriteLine("Число нечетное");
+  }
